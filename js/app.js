49fd959ae12a05b920036e679c662f6234f3da63
Marjorie Roswell <mroswell@gmail.com>
2017-11-03 14:29:23 -0700

    modify text and minZoom

--- js/app.js
+++ js/app.js
@@ -27,13 +27,13 @@ var locations = [
 ];
 
 
-var journeyMap = L.map('map').setView([46.993557,	-120.544576], 7);
+var journeyMap = L.map('map').setView([46.993557,	-120.5], 7);
 
 function loadGeo(district) {
   var Stamen_Watercolor = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}.{ext}', {
     attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
     subdomains: 'abcd',
-    minZoom: 5,
+    minZoom: 3,
     maxZoom: 14,
     ext: 'png'
   }).addTo(journeyMap)}
